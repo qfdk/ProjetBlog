42840190eaf391093066e19aee42eb61b4993aca
MA <yumeng.ma@capgemini.com>
2018-07-27 19:59:07 +0200

    [Dev]Générer le projet Blog

---
+++ blog/src/main/java/org/meng/blog/entity/Article.java
@@ -0,0 +1,48 @@
+package org.meng.blog.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String title;
+
+    private String context;
+
+
+    public Article(){
+
+    }
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContext() {
+        return context;
+    }
+
+    public void setContext(String context) {
+        this.context = context;
+    }
+}
